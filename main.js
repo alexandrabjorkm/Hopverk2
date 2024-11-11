d19456e46f14a0395ff50011dc327e7dda999779
Ólafur Sverrir Kjartansson <kjarni@gmail.com>
2024-11-11 17:10:31 +0000

    bras úr f13

--- main.js
+++ main.js
@@ -1,48 +1,26 @@
-import { renderNavigation } from './lib/components/navigation.js';
-import { el } from './lib/elements.js';
+import { fetcher } from './lib/fetcher.js';
+import { renderContentPage } from './lib/pages/content-page.js';
 import { renderIndexPage } from './lib/pages/index-page.js';
-
-async function fetchIndex() {
-  const file = 'public/data/index.json';
-
-  const response = await fetch(file);
-  const json = await response.json();
-
-  return json;
-}
-
-async function renderSubpage(root, indexJson, type) {
-  const headerElement = el('header', {}, el('h1', {}, indexJson.title));
-
-  headerElement.appendChild(renderNavigation(indexJson.navigation));
-
-  let contentString = 'EFNI ER EKKI GILT';
-
-  if (indexJson.navigation.find((i) => i.slug === type)) {
-    contentString = type;
-  }
-
-  const mainElement = el('main', {}, el('p', {}, contentString));
-
-  const footerElement = el('footer', {}, indexJson.footer);
-
-  root.appendChild(headerElement);
-  root.appendChild(mainElement);
-  root.appendChild(footerElement);
-}
+import { renderSubpage } from './lib/pages/sub-page.js';
 
 async function render(root, querystring) {
-  const indexJson = await fetchIndex();
+  const mainIndexJson = await fetcher('data/index.json');
 
   const params = new URLSearchParams(querystring);
   const type = params.get('type');
-  console.log(type);
+  const content = params.get('content');
+
+  console.log(type, content);
 
   if (!type) {
-    renderIndexPage(root, indexJson);
-  } else {
-    renderSubpage(root, indexJson, type);
+    return renderIndexPage(root, mainIndexJson);
   }
+
+  if (content) {
+    return renderContentPage(root, mainIndexJson);
+  }
+
+  renderSubpage(root, mainIndexJson, type);
 }
 
 const root = document.querySelector('#app');
